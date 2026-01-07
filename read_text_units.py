09f5137fea5d4c5beccb9f156ab61e35d1e765fb
ianfv <iandavidfairfield@gmail.com>
2026-01-07 08:55:28 -0500

    fixed parquet table reading

--- read_text_units.py
+++ read_text_units.py
@@ -1,14 +1,27 @@
 import pandas as pd
-
+from graphrag.utils.storage import load_table_from_storage
 
 GRAPHRAG_FOLDER = "test_data/output"
 
 
 def main():
     text_df = pd.read_parquet(
-        f"{GRAPHRAG_FOLDER}/text_units.parquet", columns=["id", "text", "n_tokens", "document_ids"]
+        f"{GRAPHRAG_FOLDER}/text_units.parquet",
+        columns=["id", "text", "n_tokens", "document_ids"],
+        engine="fastparquet",
     )
-    text_df.head(2)
+    print("text unit table")
+    print(text_df)
+    print("-" * 160)
+
+    entity_df = pd.read_parquet(
+        f"{GRAPHRAG_FOLDER}/entities.parquet",
+        columns=["title", "type", "description", "text_unit_ids", "degree"],
+        engine="fastparquet",
+    )
+
+    print("entitiy table")
+    print(entity_df)
 
 
 if __name__ == "__main__":
